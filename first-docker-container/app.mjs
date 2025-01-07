918b0121acaaf702c1c3d5daa1f9b7f159568f50
Arman-Kabir <armankabir8@gmail.com>
2025-01-07 09:45:57 +0600

    basic project

---
+++ first-docker-container/app.mjs
@@ -0,0 +1,15 @@
+import connectToDatabase from "./helpers.mjs";
+import express from"express";
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("<h2>Welcome to Docker</h2>");
+});
+
+await connectToDatabase();
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
